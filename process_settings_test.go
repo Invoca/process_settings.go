8bbea874c8d8b1b7ff93d2e0810f24ea93a58e58
James Ebentier <jebentier@invoca.com>
2023-04-11 21:10:38 +0200

    Update go module configuration and file structure for best practices

--- process_settings_test.go
+++ process_settings_test.go
@@ -16,27 +16,27 @@ func TestNewProcessSettingsFromFile(t *testing.T) {
 	}{
 		{
 			name:          "The file does not exist",
-			fileName:      "fixtures/does_not_exist.yml",
-			expectedError: []string{"open fixtures/does_not_exist.yml: no such file or directory"},
+			fileName:      "testdata/does_not_exist.yml",
+			expectedError: []string{"open testdata/does_not_exist.yml: no such file or directory"},
 		},
 		{
 			name:          "The file is not a valid yaml",
-			fileName:      "fixtures/invalid_yaml.yml",
+			fileName:      "testdata/invalid_yaml.yml",
 			expectedError: []string{"yaml: unmarshal errors:\n  line 5: cannot unmarshal !!map into []process_settings.SettingsFile"},
 		},
 		{
 			name:          "The file doesn't have metadata as the last element of the array",
-			fileName:      "fixtures/invalid_metadata.yml",
+			fileName:      "testdata/invalid_metadata.yml",
 			expectedError: []string{"The settings file does not have the END metadata"},
 		},
 		{
 			name:          "The file has an invalid settings file",
-			fileName:      "fixtures/invalid_settings.yml",
+			fileName:      "testdata/invalid_settings.yml",
 			expectedError: []string{"Invalid settings file at index 0: The settings file must only have settings or metadata, not both => {honeypot.yml map[] map[honeypot:map[answer_odds:100 max_recording_seconds:600 status_change_min_days:7]] {17 true}}"},
 		},
 		{
 			name:            "The file is valid",
-			fileName:        "fixtures/combined_process_settings.yml",
+			fileName:        "testdata/combined_process_settings.yml",
 			expectedSize:    6,
 			expectedVersion: 17,
 		},
@@ -57,7 +57,7 @@ func TestNewProcessSettingsFromFile(t *testing.T) {
 	}
 
 	t.Run("The settings are accessible when loaded from the file", func(t *testing.T) {
-		settings, _ := NewProcessSettingsFromFile("fixtures/combined_process_settings.yml", nil)
+		settings, _ := NewProcessSettingsFromFile("testdata/combined_process_settings.yml", nil)
 		assert.IsType(t, &[]SettingsFile{}, settings.Settings)
 		assert.Equal(t, "telecom", (*settings.Settings)[1].Target["app"])
 		assert.Equal(t, "caller_id_privacy", (*settings.Settings)[3].Settings["log_stream"].(map[string]interface{})["sip"])
